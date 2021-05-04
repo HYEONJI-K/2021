9d83b3f9fb6f935dd14ec32b88ad96c3c949c7ec
HJKIM <HJKIM@H--J>
2021-05-05 02:09:33 +0900

    Iterator Pattern

---
+++ P0320/src/BI/Aggregate.java
@@ -0,0 +1,5 @@
+package BI;
+
+public interface Aggregate {
+	public abstract Iterator iterator();
+}
---
+++ P0320/src/BI/Iterator.java
@@ -0,0 +1,6 @@
+package BI;
+
+public interface Iterator {
+	public abstract boolean hasNext();
+	public abstract Object next();
+}
